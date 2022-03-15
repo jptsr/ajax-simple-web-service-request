e0405133d22deae39dd5efd0bc083880df34eac5
Julie <juptsr.becode@gmail.com>
2022-03-15 09:30:51 +0100

    ajax method completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,40 @@
 const blockquote = document.getElementById('quote');
 
 
+// AJAX CALL
+const xhttp = new XMLHttpRequest();
+
+console.log(xhttp.readyState);
+
+xhttp.onreadystatechange = stateIsChange = () => {
+    if(xhttp.readyState == 4){
+        console.log('request completed');
+    }
+};
+
+xhttp.onload = write = () => {
+    console.log('write function is ok');
+
+    // console.log(xhttp.responseText);
+    let response = xhttp.responseText;
+    let data = JSON.parse(response);
+    console.log(data);
+    console.log(data.author);
+
+    let author = data.author;
+    blockquote.textContent = author;
+
+    console.log('end of the write function');
+};
+
+xhttp.open('GET', 'https://thatsthespir.it/api', true);
+xhttp.send();
+
+xhttp.onerror = error = () => {
+    console.log('ERROR, request failed !');
+};
+
+
 // FETCH METHOD
 // fetch('https://thatsthespir.it/api')
 // .then(res => res.json(), err => console.log(err))
@@ -25,36 +59,3 @@ const blockquote = document.getElementById('quote');
 //     console.log(data);
 //     blockquote.textContent = `Quote : ${data.quote} \n Author : ${data.author} \n Number : ${data.total_quotes}`;
 // }
-
-
-// AJAX CALL
-const xhttp = new XMLHttpRequest();
-
-xhttp.open('GET', 'https://thatsthespir.it/api', true);
-xhttp.send();
-
-xhttp.onload = write = () => {
-    console.log('write function is ok');
-
-    // console.log(this);   // return window not the value
-    // console.log(this.responseText);     // undefined
-
-    // let inJson = JSON.parse(this.responseText);
-    // console.log(inJson);
-
-    // if(this.status === 200){
-    //     console.log('ok');
-    // }else if(this.status === 403){
-    //     console.log('forbidden');
-    // }else if(this.status === 404){
-    //     console.log('page not found');
-    // }    // no status taken in account
-
-    // blockquote.textContent = this.responseText;      // nada
-
-    console.log('end of the write function');
-};
-
-xhttp.onerror = error = () => {
-    console.log('ERROR, request failed !');
-};
